1ca2f8abb31c1fc51a49a5d85a33fc61ae65fbe2
Kiran Unni <kiranunni@Kirans-MacBook-Pro.local>
2024-07-02 11:28:26 -0400

    changed button text

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,7 +41,7 @@ export default function Greeting() {
               <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
                 <Button
-                  text="See my resume"
+                  text="View my resume"
                   newTab={true}
                   href={greeting.resumeLink}
                 />
